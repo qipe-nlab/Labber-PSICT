2eceba781d3c9402dda77e259479dd48af0ffb99
sam@guadalcanal <swolski@qc.rcast.u-tokyo.ac.jp>
2018-05-14 17:43:28 +0900

    Finish testing Manual instrument
    Turns out we just need to add 10 allowed Manual channels to the RHS. We keep the generic channel names, and give them pseudo-descriptive symbols to be used in the channel relation equations.
    
    Note that a proper and thorough treatment of a custom Manual instrument is planned, but it will have to wait for the upcoming unified implementation framework.

--- parser_example.py
+++ parser_example.py
@@ -17,7 +17,7 @@ reference_path = 'C:\\Users\\qcpi\\Labber\\Data\\reference_config'
 ## Path for data; if folder does not exist, please manually create it before
 output_path = 'C:\\Users\\qcpi\\Labber\\Data\\2018\\05\\Data_0514'
 ## Config file names
-template_file_name = "manual_driver_01"
+template_file_name = "manual_driver_02"
 output_file_name = "manual_driver_out_001"
 
 FileMgr = FileManager(reference_path, template_file_name, output_path, output_file_name)
@@ -36,7 +36,7 @@ point_values = [
     "a_150e-3   w_200   v_46    s_200   p_0  f_+20   o_QubitControl",
     "a_200e-3   w_150   v_75    s_100   p_0  f_-250  o_QubitControl",
 ]
-iter_var_1 = [1, "a", 0, 150e-3, 5]
+iter_var_1 = [4, "a", 0, 150e-3, 5]
 iter_var_2 = [1, "w", 0, 300, 3]
 iter_var_3 = [1, "v", 0, 200, 6]
 iter_vars = [iter_var_3, iter_var_2, iter_var_1]
@@ -49,19 +49,16 @@ Parser.set_MeasurementObject(labber_MO, verbose = False)
 ## Apply input - point and iteration values
 Parser.set_all(point_values, iter_vars)
 
-## Set iteration variables for custom instruments (Manual, Manual2)
-##      This is done to move the parameters to the RHS so they can be used in relations
-iter_manual1 = [0, 'Value 1', 20, 21, 2]
-iter_manual2 = [0, 'Value 2', 5, 9, 5]
-Parser.set_iteration_params(iter_manual1, instrument_name = "Manual2")
-Parser.set_iteration_params(iter_manual2, instrument_name = "Manual2")
+## Update values for Manual instrument using default Labber API
+labber_MO.updateValue("Manual - Value 1", 0.5, 'SINGLE')
+labber_MO.updateValue("Manual - Value 2", 0.0, 'START')
+labber_MO.updateValue("Manual - Value 2", 5.0, 'STOP')
+labber_MO.updateValue("Manual - Value 2", 11, 'N_PTS')
 
 ## Set up Hdf5Editor object
 Editor = Hdf5Editor(labber_MO)
 
 ## Set channel spec parameters
-# Editor.add_channel_spec('man1', ["Manual2", 'Value 1', 0])
-# Editor.add_channel_spec('man2', ["Manual2", 'Value 2', 0])
 Editor.add_channel_spec('a1', ["SQPG", 'a', 1])
 Editor.add_channel_spec('w1', ["SQPG", 'w', 1])
 Editor.add_channel_spec('v1', ["SQPG", 'v', 1])
@@ -75,10 +72,16 @@ Editor.add_channel_spec('w3', ["SQPG", 'w', 3])
 Editor.add_channel_spec('v3', ["SQPG", 'v', 3])
 Editor.add_channel_spec('s3', ["SQPG", 's', 3])
 Editor.add_channel_spec('delay', ["SQPG", 'delay', 0])
+Editor.add_channel_spec('man1', ["Manual", 'Value 1', 0])
+Editor.add_channel_spec('man2', ["Manual", 'Value 2', 0])
+Editor.add_channel_spec('man3', ["Manual", 'Value 3', 0])
+Editor.add_channel_spec('man4', ["Manual", 'Value 4', 0])
+Editor.add_channel_spec('man5', ["Manual", 'Value 5', 0])
+Editor.add_channel_spec('man6', ["Manual", 'Value 6', 0])
 
 ## set relations
 Editor.set_relation(['SQPG', 'a', 2], "a1", ['a1'])
-# Editor.set_relation(['SQPG', 'w', 2], "man1 + man2", ["man1", "man2"])
+Editor.set_relation(['SQPG', 'w', 2], "man1 + man2", ["man1", "man2"])
 
 # sys.exit("Finished execution.")
 ## Run measurement

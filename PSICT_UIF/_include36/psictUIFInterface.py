150b391adf337d4b280dd428197b57c5c517ddc1
SamWolski <wlssam007@myuct.ac.za>
2018-05-27 10:38:56 +0900

    Add pulse sequence containers
    Pulse sequences will be managed by a PulseSeqManager loaded by the psictUIFInterface, which instantiates input (InputPulseSeq) and output (OutputPulseSeq) objects (both subclasses of the PulseSeq class).

--- PSICT_UIF/_include36/psictUIFInterface.py
+++ PSICT_UIF/_include36/psictUIFInterface.py
@@ -3,6 +3,7 @@
 ##  the the user directly interfaces with in the external script.
 
 from PSICT_UIF._include36.FileManager import FileManager
+from PSICT_UIF._include36.PulseSeqManager import PulseSeqManager
 
 class psictUIFInterface:
     '''
@@ -10,15 +11,24 @@ class psictUIFInterface:
     '''
 
     def __init__(self, *, verbose = 0):
+        ## NB declare all attributes explicitly for __del__ to work correctly
+        ## set logging level
         self.verbose = verbose
         ## Add constituent objects
-        ## NB declare all attributes explicitly for __del__ to work correctly
         self.fileManager = FileManager(verbose = self.verbose)
+        self.pulseSeqManager = PulseSeqManager(verbose = self.verbose)
+        ## debug message
         if self.verbose >= 4:
             print("Called psictUIFInterface constructor.")
+        ##
 
     def __del__(self):
         ## delete temporary files here (*before* deleting fileManager object!)
+        ##
+        ## delete object attributes
         del self.fileManager
+        del self.pulseSeqManager
+        ## debug message
         if self.verbose >= 4:
             print("Called psictUIFInterface destructor.")
+        ##

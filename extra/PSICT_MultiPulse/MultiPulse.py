ab59704e51ce1606619254336af851f13b6e0f61
SamWolski <wlssam007@myuct.ac.za>
2018-12-28 14:07:42 +0900

    Create MultiPulse files
    They are mostly empty at the moment.

---
+++ extra/PSICT_MultiPulse/MultiPulse.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import InstrumentDriver
+import numpy as np
+
+
+class Driver(InstrumentDriver.InstrumentWorker):
+    """ This class implements the PSICT-MultiPulse pulse generator"""
+
+
+
+
+if __name__ == '__main__':
+    pass

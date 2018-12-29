f182c06309e0081b4db4210cc5b161b6f30192cc
SamWolski <wlssam007@myuct.ac.za>
2018-12-29 17:53:31 +0900

    Add logging capabilities to MultiPulse driver

--- extra/PSICT_MultiPulse/PSICT_MultiPulse.py
+++ extra/PSICT_MultiPulse/PSICT_MultiPulse.py
@@ -3,17 +3,38 @@
 import InstrumentDriver
 import numpy as np
 
+import os
+import logging
+from datetime import datetime
 
 class Driver(InstrumentDriver.InstrumentWorker):
     """ This class implements the PSICT-MultiPulse pulse generator"""
 
     def performOpen(self, options = {}):
         '''Open the instrument connection'''
+        ## Start logging object
+        self.initLogger()
         ## Number of traces - corresponds to number of outputs
         self.nTrace = 4
         ## Waveform and time containers
         self.lWaveforms = [np.array([], dtype=float)] * self.nTrace
         self.vTime = np.array([], dtype=float)
+
+    def initLogger(self):
+        ## Dir and file setup
+        log_dir = os.path.expanduser('~/MultiPulse_logs/')
+        log_file = 'MultiPulse_{:%y%m%d_%H%M%S}'.format(datetime.now())+'.log'
+        ## Create log dir if it does not exist
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+        log_path = os.path.join(log_dir, log_file)
+        ## logger object config and init
+        logging.basicConfig(filename = log_path, filemode = 'a', \
+                            level = logging.DEBUG,\
+                            format = '%(asctime)s %(name)-8s: %(message)s', \
+                            datefmt = '%y-%m-%d %H:%M:%S')
+        self._logger = logging.getLogger('MultiPulse')
+        self._logger.info('Logging initialized to {}'.format(log_path))
 
     def performSetValue(self, quant, value, sweepRate = 0.0, options = {}):
         '''
